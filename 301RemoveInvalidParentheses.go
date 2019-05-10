6373fbbb56c1ead9ac53daba390d58f64d8a173f
Chen, Ran <rchen19@outlook.com>
2019-05-10 15:36:15 -0700

    change 301, add implementation of BFS and DFS with iteration not recursion

--- 301RemoveInvalidParentheses.go
+++ 301RemoveInvalidParentheses.go
@@ -27,12 +27,219 @@ import (
 	"fmt"
 )
 
+//using BFS iteration
 func removeInvalidParentheses(s string) []string {
 	var out []string
+	var currNode node
+	var count int
+	var toPaseRightToLeft bool
+	var goodToGo bool
+	//use a queue to keep the nodes to be explored
+	//first in first out to achieve BFS
+	var queue []node = []node{node{s, 0, 0, true}}
+	for len(queue) > 0 {
+		//pop the last node from the stack
+		currNode = queue[0]
+		queue = queue[1:]
+		if currNode.leftToRight {
+			//parse left->right, check for excess ')'
+			toPaseRightToLeft = true
+			count = 0
+			for i := currNode.i; i < len(currNode.s); i++ {
+				switch currNode.s[i] {
+				case '(':
+					count++
+				case ')':
+					count--
+				}
+				//when never count<0, need to remove a ')'
+				if count < 0 {
+					toPaseRightToLeft = false
+					//since there exist excess ')', no need to parse right->left
+					for j := currNode.j; j <= i; j++ {
+						if currNode.s[j] == ')' && (j == currNode.j || currNode.s[j-1] != ')') {
+							queue = append(queue, node{currNode.s[:j] + currNode.s[j+1:], i, j, currNode.leftToRight})
+						}
+					}
+					break
+				}
+			}
+			//if looks good from left to right
+			//push right to left node to the stack
+			//finished parsing from left to right
+			if toPaseRightToLeft {
+				queue = append(queue, node{currNode.s, len(currNode.s) - 1, len(currNode.s) - 1, false})
+			}
+		} else {
+			goodToGo = true
+			count = 0
+			//parse from right to left
+			//check for excess '('
+			for i := currNode.i; i >= 0; i-- {
+				switch currNode.s[i] {
+				case '(':
+					count--
+				case ')':
+					count++
+				}
+				//when never count<0, need to remove a ')'
+				if count < 0 {
+					goodToGo = false
+					for j := currNode.j; j >= i; j-- {
+						if currNode.s[j] == '(' && (j == currNode.j || currNode.s[j+1] != '(') {
+							queue = append(queue, node{currNode.s[:j] + currNode.s[j+1:], i - 1, j - 1, currNode.leftToRight})
+						}
+					}
+					break
+				}
 
-	genValid(s, &out, 0, 0, []byte{'(', ')'})
+			}
+			//finished parsing from right to left
+			if goodToGo {
+				out = append(out, currNode.s)
+			}
+
+		}
+
+	}
 
 	return out
+}
+
+//using DFS iteration
+func removeInvalidParentheses1(s string) []string {
+	var out []string
+	var currNode node
+	var count int
+	var toPaseRightToLeft bool
+	var goodToGo bool
+	//use a stack to keep the nodes to be explored
+	//last in first out to achieve DFS
+	var stack []node = []node{node{s, 0, 0, true}}
+	for len(stack) > 0 {
+		//pop the last node from the stack
+		currNode = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if currNode.leftToRight {
+			//parse left->right, check for excess ')'
+			toPaseRightToLeft = true
+			count = 0
+			for i := currNode.i; i < len(currNode.s); i++ {
+				switch currNode.s[i] {
+				case '(':
+					count++
+				case ')':
+					count--
+				}
+				//when never count<0, need to remove a ')'
+				if count < 0 {
+					toPaseRightToLeft = false
+					//since there exist excess ')', no need to parse right->left
+					for j := currNode.j; j <= i; j++ {
+						if currNode.s[j] == ')' && (j == currNode.j || currNode.s[j-1] != ')') {
+							stack = append(stack, node{currNode.s[:j] + currNode.s[j+1:], i, j, currNode.leftToRight})
+						}
+					}
+					break
+				}
+			}
+			//if looks good from left to right
+			//push right to left node to the stack
+			//finished parsing from left to right
+			if toPaseRightToLeft {
+				stack = append(stack, node{currNode.s, len(currNode.s) - 1, len(currNode.s) - 1, false})
+			}
+		} else {
+			goodToGo = true
+			count = 0
+			//parse from right to left
+			//check for excess '('
+			for i := currNode.i; i >= 0; i-- {
+				switch currNode.s[i] {
+				case '(':
+					count--
+				case ')':
+					count++
+				}
+				//when never count<0, need to remove a ')'
+				if count < 0 {
+					goodToGo = false
+					for j := currNode.j; j >= i; j-- {
+						if currNode.s[j] == '(' && (j == currNode.j || currNode.s[j+1] != '(') {
+							stack = append(stack, node{currNode.s[:j] + currNode.s[j+1:], i - 1, j - 1, currNode.leftToRight})
+						}
+					}
+					break
+				}
+
+			}
+			//finished parsing from right to left
+			if goodToGo {
+				out = append(out, currNode.s)
+			}
+
+		}
+
+	}
+
+	return out
+}
+
+//define a custom type to store strings and indices and direction to push to stack
+type node struct {
+	s           string
+	i           int
+	j           int
+	leftToRight bool
+}
+
+//using DFS recursion
+func removeInvalidParentheses2(s string) []string {
+	var out []string
+
+	//genValid(s, &out, 0, 0, []byte{'(', ')'})
+	genValid1(s, &out, 0, 0, true)
+
+	return out
+}
+
+func genValid1(s string, out *[]string, last_i int, last_j int, leftToRight bool) {
+	//set a counter, ++ when seeing '(', and -- when seeing ')'
+	var count int = 0
+	for i := last_i; i < len(s); i++ {
+		if s[i] == '(' {
+			count++
+		} else if s[i] == ')' {
+			count--
+		}
+		//when never count<0, need to remove a ')'
+		if count < 0 {
+			for j := last_j; j <= i; j++ {
+				//find ')' between index last_j->i
+				//only try ')' whose previous character is not ')'
+				//to avoid duplicate results
+				if s[j] == ')' && (j == last_j || s[j-1] != ')') {
+					//recursively find valid on s with ')' removed
+					genValid1(s[:j]+s[j+1:], out, i, j, leftToRight)
+				}
+			}
+			return
+		}
+	}
+
+	//if there do not exist excess ')'
+	//check to see if there exist excess '('
+	//by traversing s from right to left
+	var sReversed string = reverseString1(s)
+
+	if leftToRight {
+		//finished left to right
+		//now start right to left
+		genValid1(sReversed, out, 0, 0, false)
+	} else {
+		//finished right to left
+		*out = append(*out, sReversed)
+	}
 }
 
 func genValid(s string, out *[]string, last_i int, last_j int, par []byte) {
@@ -58,15 +265,15 @@ func genValid(s string, out *[]string, last_i int, last_j int, par []byte) {
 	} else {
 		*out = append(*out, sReversed)
 	}
-
 }
 
+//this function reverse the string
+//also swap '(' and ')'
 func reverseString1(s string) string {
 	var newStr = make([]rune, len(s))
 	for i, char := range s {
 		switch char {
 		case '(':
-			//fmt.Println(len(s), i)
 			newStr[len(s)-1-i] = ')'
 		case ')':
 			newStr[len(s)-1-i] = '('
@@ -77,6 +284,7 @@ func reverseString1(s string) string {
 	return string(newStr)
 }
 
+//this function reverse the string
 func reverseString(s string) string {
 	if len(s) < 2 {
 		return s
@@ -89,6 +297,6 @@ func reverseString(s string) string {
 }
 
 func main() {
-	var s string = "(a)())()"
+	var s string = "(()a"
 	fmt.Println(removeInvalidParentheses(s))
 }
